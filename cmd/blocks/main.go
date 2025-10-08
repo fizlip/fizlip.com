6267d82c64afed7671a178161cf97a4543dcdc63
fizlip <fillatino@gmail.com>
2025-10-08 14:23:14 +0200

    first commit

---
+++ cmd/blocks/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
+func main() {
+	e := echo.New()
+	e.Renderer = NewTemplates()
+	e.Use(middleware.Logger())
+
+	// Serve CSS files from /css
+	e.Static("/css", "css")
+
+	count := Count{Count: 0}
+
+	e.GET("/", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index", count)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
